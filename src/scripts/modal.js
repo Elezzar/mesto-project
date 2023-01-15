fd68db530bcf45dc9824dbaa4a70a2f77c27a9f9
Elezzar <nikolassion@yandex.ru>
2023-01-15 15:51:53 +0700

    refactoring/использование функции closePopup внутри функций closePressEsc и closeClickOverlay + замена события click на mousedown

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -5,18 +5,18 @@ import { body, content } from './utils.js'
 
 /**закрытие модального окна через клавишу escape */
 function closePressEsc (evt) {
-  /** открытое модальное окно*/
-  const openedPopup = content.querySelector('.popup_opened');
-
   if(evt.key === 'Escape'){
-    openedPopup.classList.remove('popup_opened');
+    /** открытое модальное окно*/
+    const openedPopup = content.querySelector('.popup_opened');
+    closePopup(openedPopup);
   };
 };
 
 /**закрытие модального окна через клик по overlay */
 function closeClickOverlay (element) {
   if(element.target.classList.contains('popup')){
-    element.target.classList.remove('popup_opened');
+    const openedPopup = content.querySelector('.popup_opened');
+    closePopup(openedPopup);
   };
 };
 
@@ -25,7 +25,7 @@ function closeClickOverlay (element) {
 function openPopup(element) {
   element.classList.add('popup_opened');
 
-  body.addEventListener('click', closeClickOverlay);
+  body.addEventListener('mousedown', closeClickOverlay);
   body.addEventListener('keydown', closePressEsc);
 };
 
@@ -33,7 +33,7 @@ function openPopup(element) {
 function closePopup(element) {
   element.classList.remove('popup_opened');
 
-  body.removeEventListener('click', closeClickOverlay);
+  body.removeEventListener('mousedown', closeClickOverlay);
   body.removeEventListener('keydown', closePressEsc);
 };
 
